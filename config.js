e687f6f9c3b863ad5268d94496828e8b52143bfb
yony-bentancor <yonybentancor@gmail.com>
2023-12-26 19:48:18 -0300

    UPDATE

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ require("dotenv").config(); // Cargar variables de entorno desde el archivo .env
 
 const CLAVE_SECRETA = process.env.CLAVE_SECRETA; /*  || "dulce29"; */
 const PORT = process.env.PORT; /*  || 80; */
-const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING; /*  ||
-  "mongodb+srv://proyectodulce:dulce29@cluster0.bi9aze0.mongodb.net/?retryWrites=true&w=majority"; */
+const DB_CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING ||
+  "mongodb+srv://proyectodulce:dulce29@cluster0.bi9aze0.mongodb.net/?retryWrites=true&w=majority";
 
 const uri =
   "mongodb+srv://proyectodulce:dulce29@cluster0.bi9aze0.mongodb.net/?retryWrites=true&w=majority";
