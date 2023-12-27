8ff59e93025efcf8ace6e5932eb77708160b101a
yony-bentancor <yonybentancor@gmail.com>
2023-12-26 22:47:23 -0300

    update

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,6 +1,6 @@
 const Usuario = require("../models/Usuario");
 const bcrypt = require("bcrypt");
-const jwt = require("n");
+const jwt = require("jsonwebtoken");
 const { CLAVE_SECRETA } = require("../config");
 
 module.exports = {
@@ -13,25 +13,42 @@ module.exports = {
 
   login: async (req, res) => {
     try {
-      const userInfo = req.body;
-      password = userInfo.username;
-      const hash = await bcrypt.hash(password, saltRounds);
-      const newUser = await Usuario.create({
-        username: userInfo.username,
-        hash: hash,
-      });
+      const user = req.body;
+      const newUser = await Usuario.findOne({ username: user.username });
+
+      if (!newUser) {
+        return res.redirect(
+          "https://dulce29.herokuapp.com/administrador?error=UsuarioIncorrecto"
+        );
+      }
+
+      const match = await bcrypt.compare(user.password, newUser.hash);
+
+      if (!match) {
+        return res.redirect(
+          "https://dulce29.herokuapp.com/administrador?error=ContraseñaIncorrecta"
+        );
+      }
 
       const userRes = {
         username: newUser.username,
         id: newUser.id,
       };
-      const token = jwt.sign(userRes, CLAVE_SECRETA);
 
-      res.redirect("/home");
+      if (userRes.username === "ADMINISTRADOR") {
+        const token = jwt.sign(userRes, CLAVE_SECRETA);
 
-      /*res.status(201).json({ useer: userRes, token: token });*/
+        // Nuevo código para el caso de usuario "ADMINISTRADOR"
+        try {
+          const users = await Usuario.find().sort({ username: 1 });
+          return res.render("clientes", { users, token });
+        } catch (error) {
+          return res.status(500).json({ error: error.message });
+        }
+      }
     } catch (error) {
-      res.status(400).json({ error: error.message });
+      // Manejo de errores generales
+      return res.status(500).json({ error: error.message });
     }
   },
 };
